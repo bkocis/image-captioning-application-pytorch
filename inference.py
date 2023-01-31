f62d628664d8d46055fc9a7a07fe5a76c14792b3
Balazs.Kocsis <balaz.kocis@gmail.com>
2023-01-31 23:53:08 +0100

    fastapi implementation

--- inference.py
+++ inference.py
@@ -7,58 +7,82 @@ from utils.model import EncoderCNN, DecoderRNN
 from utils.data_loader import get_loader
 from torchvision import transforms
 
-transform_test = transforms.Compose([
-                            transforms.Resize(256),                          # smaller edge of image resized to 256
-                            transforms.RandomCrop(224),                      # get 224x224 crop from random location
-                            transforms.RandomHorizontalFlip(),               # horizontally flip image with probability=0.5
-                            transforms.ToTensor(),                           # convert the PIL Image to a tensor
-                            transforms.Normalize((0.485, 0.456, 0.406),      # normalize image for pre-trained model
-                                                 (0.229, 0.224, 0.225))])
 
-data_loader = get_loader(transform=transform_test,
-                         mode='test')
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+class InferenceOnSingleImage:
 
-encoder_file = "encoder_n2-3.pkl"
-decoder_file = "decoder_n2-3.pkl"
+    # encoder_file = "encoder_n2-3.pkl"
+    # decoder_file = "decoder_n2-3.pkl"
+    # embed_size = 300
+    # hidden_size = 300
 
-embed_size = 300
-hidden_size = 300
+    def __int__(self, encoder_file, decoder_file=None, embed_size=None, hidden_size=None, device=None):
 
-vocab_size = len(data_loader.dataset.vocab)
+        self.encoder_file = "encoder_n2-3.pkl"
+        self.decoder_file = "decoder_n2-3.pkl"
+        self.embed_size = 300
+        self.hidden_size = 300
 
-encoder = EncoderCNN(embed_size)
-encoder.eval()
-decoder = DecoderRNN(embed_size, hidden_size, vocab_size)
-decoder.eval()
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-encoder.load_state_dict(torch.load(os.path.join('./models', encoder_file)))
-decoder.load_state_dict(torch.load(os.path.join('./models', decoder_file)))
+    # def transform_test(self):
+    #     transform_test = transforms.Compose([
+    #                                 transforms.Resize(256),                          # smaller edge of image resized to 256
+    #                                 transforms.RandomCrop(224),                      # get 224x224 crop from random location
+    #                                 transforms.RandomHorizontalFlip(),               # horizontally flip image with probability=0.5
+    #                                 transforms.ToTensor(),                           # convert the PIL Image to a tensor
+    #                                 transforms.Normalize((0.485, 0.456, 0.406),      # normalize image for pre-trained model
+    #                                                      (0.229, 0.224, 0.225))])
+    #     return transform_test
 
-encoder.to(device)
-decoder.to(device)
+    def load_enc_dec(self, vocab_size):
+        encoder = EncoderCNN(self.embed_size)
+        encoder.eval()
+        decoder = DecoderRNN(self.embed_size, self.hidden_size, vocab_size)
+        decoder.eval()
 
+        encoder.load_state_dict(torch.load(os.path.join('./models', self.encoder_file)))
+        decoder.load_state_dict(torch.load(os.path.join('./models', self.decoder_file)))
 
-def clean_sentence(output):
-    words = [data_loader.dataset.vocab.idx2word[i] for i in output][1:-1]
-    sentence = ' '.join(words)
-    return sentence
+        encoder.to(self.device)
+        decoder.to(self.device)
+        return encoder, decoder
 
+    def load_data(self):
+        transform_test = transforms.Compose([
+                                    transforms.Resize(256),                          # smaller edge of image resized to 256
+                                    transforms.RandomCrop(224),                      # get 224x224 crop from random location
+                                    transforms.RandomHorizontalFlip(),               # horizontally flip image with probability=0.5
+                                    transforms.ToTensor(),                           # convert the PIL Image to a tensor
+                                    transforms.Normalize((0.485, 0.456, 0.406),      # normalize image for pre-trained model
+                                                         (0.229, 0.224, 0.225))])
+        data_loader = get_loader(transform=transform_test, mode='test')
+        vocab_size = len(data_loader.dataset.vocab)
+        orig_image, image = next(iter(data_loader))
+        return image, vocab_size
 
-def get_prediction():
-    orig_image, image = next(iter(data_loader))
-    plt.imshow(np.squeeze(orig_image))
-    plt.title('Sample Image')
-    # plt.show()
-    image = image.to(device)
+    def clean_sentence(self, output):
+        words = [self.data_loader.dataset.vocab.idx2word[i] for i in output][1:-1]
+        sentence = ' '.join(words)
+        return sentence
 
-    # Obtain the embedded image features.
-    features = encoder(image).unsqueeze(1)
+    def get_prediction(self, image, encoder, decoder):
 
-    # Pass the embedded image features through the model to get a predicted caption.
-    output = decoder.sample(features)
-    sentence = clean_sentence(output)
-    plt.savefig(f"resources/Sample_image-{sentence}.png")
+        # plt.imshow(np.squeeze(orig_image))
+        # plt.title('Sample Image')
+        # plt.show()
+        image = image.to(self.device)
 
-get_prediction()
+        # Obtain the embedded image features.
+        features = encoder(image).unsqueeze(1)
 
+        # Pass the embedded image features through the model to get a predicted caption.
+        output = decoder.sample(features)
+
+        sentence = self.clean_sentence(output)
+        plt.savefig(f"resources/Sample_image-{sentence}.png")
+        return sentence
+
+    def caption_sentence(self):
+        image, vocab_size = self.load_data()
+        encoder, decoder = self.load_enc_dec(vocab_size)
+        self.get_prediction(image, encoder, decoder)
--- main.py
+++ main.py
@@ -1,96 +1,42 @@
 import os
-import nltk
-import torch
-import torch.utils.data as data
+import uvicorn
+import logging
 
-from torchvision import transforms
-from pycocotools.coco import COCO
-from collections import Counter
+from fastapi import FastAPI, File, UploadFile
 
-from utils.model import EncoderCNN
-from utils.data_loader import get_loader
+from inference import InferenceOnSingleImage
 
-nltk.download('punkt')
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+
+app = FastAPI(docs_url="/")
 
 
-def coco_api_init():
-    # initialize COCO API for instance annotations
-    dataDir = '/home/snow/Documents/Datasets/cocoapi'
-    dataType = 'val2017'
-    instances_annFile = os.path.join(dataDir, f'annotations/instances_{dataType}.json')
-    coco = COCO(instances_annFile)
+@app.post("/upload_video/")
+async def image_file(image_file: UploadFile = File(...)):
+    logging.info(image_file.file)
+    try:
+        os.mkdir("resources/image_file")
 
-    # initialize COCO API for caption annotations
-    captions_annFile = os.path.join(dataDir, f'annotations/captions_{dataType}.json')
-    coco_caps = COCO(captions_annFile)
+        logging.info(os.getcwd())
+    except Exception as e:
+        logging.info(e)
 
-    # get image ids
-    ids = list(coco.anns.keys())
+    file_path = os.path.join(os.getcwd(), "resources/image_file", image_file.filename.replace(" ", "-"))
+    with open(file_path, 'wb+') as f:
+        f.write(image_file.file.read())
 
+    result = ...
 
-def initialize_data_loader():
-    # Define a transform to pre-process the training images.
-    transform_train = transforms.Compose([
-        transforms.Resize(256),  # smaller edge of image resized to 256
-        transforms.RandomCrop(224),  # get 224x224 crop from random location
-        transforms.RandomHorizontalFlip(),  # horizontally flip image with probability=0.5
-        transforms.ToTensor(),  # convert the PIL Image to a tensor
-        transforms.Normalize((0.485, 0.456, 0.406),  # normalize image for pre-trained model
-                             (0.229, 0.224, 0.225))])
+    output = {
+        "filename": image_file.filename,
+        "predicted_caption": result
+    }
 
-    # Set the minimum word count threshold.
-    vocab_threshold = 5
+    logging.info(f"Prediction for {image_file} ...done!")
+    return output
 
-    # Specify the batch size.
-    batch_size = 10
-
-    # Obtain the data loader.
-    data_loader = get_loader(transform=transform_train,
-                             mode='train',
-                             batch_size=batch_size,
-                             vocab_threshold=vocab_threshold,
-                             vocab_from_file=False)
-
-    counter = Counter(data_loader.dataset.caption_lengths)
-    lengths = sorted(counter.items(), key=lambda pair: pair[1], reverse=True)
-    indices = data_loader.dataset.get_train_indices()
-    new_sampler = data.sampler.SubsetRandomSampler(indices=indices)
-    data_loader.batch_sampler.sampler = new_sampler
-
-    # # Obtain the batch.
-    # images, captions = next(iter(data_loader))
-    # return images, captions
-
-
-def experiment_with_cnn():
-    # Specify the dimensionality of the image embedding.
-    embed_size = 256
-
-    # -#-#-# Do NOT modify the code below this line. #-#-#-#
-
-    # Initialize the encoder. (Optional: Add additional arguments if necessary.)
-    encoder = EncoderCNN(embed_size)
-
-    # Move the encoder to GPU if CUDA is available.
-    encoder.to(device)
-
-    # Move last batch of images (from Step 2) to GPU if CUDA is available.
-    images = images.to(device)
-
-    # Pass the images through the encoder.
-    features = encoder(images)
-
-    print('type(features):', type(features))
-    print('features.shape:', features.shape)
-
-    # Check that your encoder satisfies some requirements of the project!
-    assert type(features) == torch.Tensor, "Encoder output needs to be a PyTorch Tensor."
-    assert (features.shape[0] == batch_size) & (
-                features.shape[1] == embed_size), "The shape of the encoder output is incorrect."
-
-
-if __name__ == '__main__':
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    coco_api_init()
-    # initialize_data_loader()
+if __name__ == "__main__":
+    get_caption = InferenceOnSingleImage().caption_sentence()
+    logging.info("The service is starting...")
+    get_caption()
+    uvicorn.run(app, host="0.0.0.0", port=8080)
