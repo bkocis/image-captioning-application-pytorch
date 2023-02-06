4f0d85f26357839a429224793c5d2cc0a47c97c8
Balazs.Kocsis <balaz.kocis@gmail.com>
2023-02-06 23:51:30 +0100

    remove some image transformation steps in inference

--- application/inference.py
+++ application/inference.py
@@ -75,8 +75,6 @@ class InferenceOnSingleImage:
     def transform_image(self, image):
         transform_img = transforms.Compose([
                 transforms.Resize(256),                          # smaller edge of image resized to 256
-                transforms.RandomCrop(224),                      # get 224x224 crop from random location
-                transforms.RandomHorizontalFlip(),               # horizontally flip image with probability=0.5
                 transforms.ToTensor(),                           # convert the PIL Image to a tensor
                 transforms.Normalize((0.485, 0.456, 0.406),      # normalize image for pre-trained model
                                      (0.229, 0.224, 0.225))])
