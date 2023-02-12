18abaef44cab5f60e5f6430bd38ce7de7a3b8732
Balazs.Kocsis <balaz.kocis@gmail.com>
2023-02-12 16:18:20 +0100

    tagging restapi

--- application/main.py
+++ application/main.py
@@ -1,5 +1,4 @@
 import os
-import uvicorn
 import logging
 from PIL import Image
 from fastapi import FastAPI, File, UploadFile
@@ -10,10 +9,40 @@ from application.inference import InferenceOnSingleImage
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
-app = FastAPI(docs_url="/")
+app = FastAPI(docs_url="/",
+              title="Image Captioning Application",
+              description="This is a simple FastAPI application for image captioning",
+              )
 
 
-@app.post("/upload_image/")
+@app.post("/caption_image/", tags=["Image Captioning"])
+async def image_file(image_file: UploadFile = File(...)):
+    logging.info(image_file.file)
+    try:
+        os.mkdir("./resources/image_file")
+
+        logging.info(os.getcwd())
+    except Exception as e:
+        logging.info(e)
+
+    file_path = os.path.join(os.getcwd(), "./resources/image_file", image_file.filename.replace(" ", "-"))
+    with open(file_path, 'wb+') as f:
+        f.write(image_file.file.read())
+
+    image = Image.open(os.path.join(file_path)).convert('RGB')
+    get_caption = InferenceOnSingleImage()
+    orig_image, sentence = get_caption.caption_sentence_from_upload(image)
+
+    output = {
+        "filename": image_file.filename,
+        "predicted_caption": sentence
+    }
+
+    logging.info(f"Prediction for {image_file} ...done!")
+    return output
+
+
+@app.post("/upload_image/", tags=["Image Captioning"])
 async def image_file(image_file: UploadFile = File(...)):
     logging.info(image_file.file)
     try:
@@ -42,8 +71,3 @@ async def image_file(image_file: UploadFile = File(...)):
     buf.seek(0)
     logging.info(f"Prediction for {image_file} ...done!")
     return StreamingResponse(content=buf, media_type="image/jpeg", headers={"Content-Disposition": f"{output}"})
-
-# if __name__ == "__main__":
-#     get_caption = InferenceOnSingleImage()
-#     logging.info("The service is starting...")
-#     uvicorn.run(app, host="0.0.0.0", port=8081)
